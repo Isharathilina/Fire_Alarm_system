27bbc8bd28593c8f8e049ef4fca4a98cae038bc4
isharathilina <isharawap@.com>
2019-10-22 14:56:14 +0530

    second edit

--- final_concat_and_send_sms/final_concat_and_send_sms.ino
+++ final_concat_and_send_sms/final_concat_and_send_sms.ino
@@ -1,125 +1,110 @@
-/*
+/* LCD display pin configuration
+
  * LCD RS pin to digital pin 7
  * LCD Enable pin to digital pin 6
  * LCD D4 pin to digital pin 5
  * LCD D5 pin to digital pin 4
  * LCD D6 pin to digital pin 3
  * LCD D7 pin to digital pin 2
- 
  */
+#define PrintSerialMonitor  //if define it, print on serial monitor 
+#define SkipVerify    // if define it, skip phone number verify step 
 
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial sim(10, 11);
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);  //rs, en , d4 to d7
+SoftwareSerial sim(10, 11);  //communicate for gsm
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);  //pin connect(rs, en , d4 to d7)
 
-//String inputString = "";   
-bool inputComplete = false; // for interupt
+  // for interupt serial data
+String inputSerialData = "";
+bool inputComplete = false; 
 
-String contact[2]={"N","N"};
-String defaltmsg = "Emergency";
-String testmsg = "Testing Ok";
+String contact[2]={"N","N"};  // for store contact numbers
+String emergencytext = "Emergency";  //emergency msg
+String testmsg = "Testing Ok";  // test msg
 
 void setup() {
-   lcd.begin(16, 2);
-   Serial.begin(9600);
-   sim.begin(9600);
-
-   lcd.setCursor(2, 0);
-   lcd.print("..Welcome..");
-   delay(2000);
-
-
+  
+  Serial.begin(9600);
+  sim.begin(9600);
+  
+  lcd.begin(16, 2);  // start lcd
+  lcd.setCursor(2, 0);
+  lcd.print("..Welcome..");
+  delay(2000);
    
-//------------------------- Initialize GSM
-   lcd.clear();
-   lcd.print("Initialize GSM");
-   delay(1000);
 
-   if(InitGsm()==1){
-       lcd.clear();
-       lcd.print("GSM OK");
-       delay(1000);
-      
-    }else{
-      
-      while(1){
-         lcd.clear();
-         lcd.print("GSM Fail");
-         delay(500);
-        
-        }
-      }
-
- //--------------------------Reading contact
-
+    //Initialize GSM--------------------
+  lcd.clear();
+  lcd.print("Initialize GSM");
+  delay(1000);
+  
+  if(InitGsm()==1){
     lcd.clear();
-    lcd.print("Reading Contact");
-    delay(500);
-
-if(StoreContact()==1){
-       lcd.clear();
-       lcd.print("Contacts OK");
-       delay(500);
-      
-    }else{
-      while(1){
-        
-       lcd.clear();
-       lcd.print("Contacts Fail");
-       delay(500);
-        
-        }
+    lcd.print("GSM OK");
+    delay(1000);
+  }else{
+    while(1){
+      lcd.clear();
+      lcd.print("GSM Fail");
+      delay(500);
       }
-
-//----------------------------- check network status 
-
- lcd.clear();
-    lcd.print("Connectig..");
+    }
+    
+    //Reading contact-------------------
+  lcd.clear();
+  lcd.print("Reading Contact");
+  delay(500);
+  
+  if(StoreContact()==1){
+    lcd.clear();
+    lcd.print("Contacts OK");
     delay(500);
-
-if(CheckNetwork()==1){
-       lcd.clear();
-       lcd.print("Network conected");
-       delay(500);
-      
-    }else{
-      while(1){
-        
-       lcd.clear();
-       lcd.print("Network Fail");
-       delay(500);
-        
-        }
+  }else{
+    while(1){
+      lcd.clear();
+      lcd.print("Contacts Fail");
+      delay(500);
       }
+    }
 
-  //----------------------------verify contact number(sending test contact)
+    //check network status---------------
+  lcd.clear();
+  lcd.print("Connectig..");
+  delay(500);
+  
+  if(CheckNetwork()==1){
+    lcd.clear();
+    lcd.print("Network conected");
+    delay(500);
+  }else{
+    while(1){
+      lcd.clear();
+      lcd.print("Network Fail");
+      delay(500);
+      }
+    }
 
- /*
-    
-     lcd.clear();
-        lcd.print("veryfing Numbers");
-        delay(500);
-    
-    if(veryfy()==1){
-           lcd.clear();
-           lcd.print("Verified");
-           delay(500);
-          
-        }else{
-          while(1){
-            
-           lcd.clear();
-           lcd.print("Verified Fail");
-           delay(500);
-            
-            }
-          }
+#if !defined(SkipVerify)
+    //verify contact number(sending test contact)    
+  lcd.clear();
+  lcd.print("veryfing Numbers");
+  delay(500);
+  
+  if(veryfy()==1){
+    lcd.clear();
+    lcd.print("Verified");
+    delay(500);
+  }else{
+    while(1){
+      lcd.clear();
+      lcd.print("Verified Fail");
+      delay(500);
+      }
+    }
+#endif
 
-
-
-   */
 
 //Serial.println(InitGsm());  // testing gsm return 1 for sucsess
 //Serial.println(StoreContact());  //store contact to contact arry and if sucsess return 1
@@ -128,233 +113,353 @@ if(CheckNetwork()==1){
 // Readcontact(2);
 //Sendsms(contact[0],defaltmsg);
 //Sendsms(contact[1],defaltmsg);
-// GetsingalStrength();
+// GetsingalStrength(); 
 
- 
+  lcd.clear();
 }
-
-
-
-
 
 void loop() {
-           String st = GetsingalStrength();
-           lcd.clear();
-           lcd.print("Signal Strength");           
-           lcd.setCursor(6, 1);
-           lcd.print(st);
-           Serial.println(st);
-           delay(100);
-
   
-
+  ShowSignalBar(GetsingalStrength()); // show siganl bar with current strength
   
+  lcd.setCursor(0, 1);
+  lcd.print("Operating.");
+  delay(100);
+  lcd.print(".");
+  delay(100);
+  lcd.print(".");
+  delay(100);
+  lcd.print(".");
+  delay(100);
+  
+  if(inputComplete){
+    lcd.clear();
+    lcd.print("Data coming");
+    delay(500);
 
-if (inputComplete) {
-    Serial.println("Interupted something");
-
-           lcd.clear();
-           lcd.print("Data coming"); 
-           delay(1000);
+    //inputSerialData  -> this variable have serial data 
+    //Serial.println("Interupted something");   
+    //Serial.println(inputSerialData.charAt(20));
+    String Firedata = inputSerialData.substring(22, 32);
     
+    if(!(Firedata.indexOf("fire")==-1)){  // if serial data have "fire" string, should send sms
+    
+      Sendsms(contact[0],emergencytext);
+      Sendsms(contact[1],emergencytext);
+      
+      lcd.clear();
+      lcd.print("Sent SMS");
+      delay(1000);
+      
+    }else{
+      //if haven't identify fire data
+      lcd.clear();
+      lcd.print("You are safe");
+      delay(1000);
+      }
+      
     inputComplete = false;
+    inputSerialData ="";
   }
-
-
+  
 }
 
 
-String GetsingalStrength(){
+void ShowSignalBar(int strength){
+
+  // Custom charactors data
+  byte Signalbar[] = {
+  B11111,
+  B10101,
+  B01110,
+  B00100,
+  B00100,
+  B00100,
+  B00100,
+  B00100
+  };
+
+
+  byte sig4[] = {
+  B11000,
+  B11000,
+  B11000,
+  B11000,
+  B11000,
+  B11000,
+  B11000,
+  B11000
+  };
+
+  byte sig3[] = {
+  B00000,
+  B00000,
+  B11000,
+  B11000,
+  B11000,
+  B11000,
+  B11000,
+  B11000
+  };
+
+  byte sig2[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11000,
+  B11000,
+  B11000,
+  B11000
+  };
+
+  byte sig1[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11000,
+  B11000
+  };
+
+  //Create custom charactors
+  lcd.createChar(0, Signalbar);
+  lcd.createChar(1, sig1);
+  lcd.createChar(2, sig2);
+  lcd.createChar(3, sig3);
+  lcd.createChar(4, sig4);
+  
+  lcd.clear();
+  lcd.setCursor(11, 0); // battery bar cordinate
+  
+  lcd.write(byte(0));   //show signal tower
+  
+  if(strength<10){    //show signl bars
+    lcd.write(byte(1));
+  
+  }else if(strength<15){    
+      lcd.write(byte(1));
+      lcd.write(byte(2));
+  
+  }else if(strength<20){
+  
+      lcd.write(byte(1));
+      lcd.write(byte(2));
+      lcd.write(byte(3)); 
+  
+  }else if(strength<30){
+  
+      lcd.write(byte(1));
+      lcd.write(byte(2));
+      lcd.write(byte(3));
+      lcd.write(byte(4));
+
+      }
+}
+
+
+void serialEvent(){
+
+  String response;
+  
+  while (Serial.available()){
+    response.concat((char)sim.read());
+  }
+  
+  inputSerialData = response;
+  inputComplete = true;
+  
+  
+  /*
+  
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    inputSerialData += inChar;
+  
+    if(inChar == '\n'){
+    stringComplete = true;
+    }
+    
+  }
+  */
+}
+
+
+int GetsingalStrength(){
 
   sim.print("AT\r\n");
   delay(500);
   while(!sim.available());  // for ready gsm
-
-  while(sim.available()){
-   //Serial.print((char)sim.read());
-   int temp = (char)sim.read();    // get garbadge values
-  } 
   
-  sim.print("AT+CSQ\r\n"); 
-  delay(500);
-  while(!sim.available());
-
-  String response;
   while(sim.available()){
-  response.concat((char)sim.read());
+    //Serial.print((char)sim.read());
+    int temp = (char)sim.read();  // get garbadge values
     
-  }
-//Serial.println(response);                     // strength format  +CSQ: 4,0  
- int a = response.indexOf(' ');
- int b = response.indexOf(',');
- 
- String strength = response.substring(a+1, b);  // strength value
-
- Serial.print("Signal strength:- ");
- Serial.println(strength);
-  
-  return strength;
-  
-  
-  }
-
-
-
-int veryfy(){ 
-  
-  int n1 = Sendsms(contact[0],testmsg);    // send sms and get return (if sent, return 1)
-  int n2 = Sendsms(contact[1],testmsg);
-  if(n1==1 and n2==1 ){
-
-    Serial.println("numbers veryfied");
-    return 1;
-    }else {
-        Serial.println("numbers veryfy fail");
-      return 0;
-      }
-  
-  }
-
-
-void serialEvent() {
-  String response;
-  while (Serial.available()) { 
-  response.concat((char)sim.read());
- 
-  }
-  
-     Serial.println(response);  
-      inputComplete = true;
     }
- 
+    
+  sim.print("AT+CSQ\r\n");
+  delay(500);
+  
+  while(!sim.available());
+  
+  String response;
+  while(sim.available()){
+  response.concat((char)sim.read());
+  
+  }
+  
+  //Serial.println(response);   // strength format  +CSQ: 4,0
+  int a = response.indexOf(' ');
+  int b = response.indexOf(',');
+  
+  String strength = response.substring(a+1, b);   // strength value
+ #if defined(PrintSerialMonitor)
+  Serial.print("Signal strength:- ");
+  Serial.println(strength);
+ #endif
+  
+  return strength.toInt();
+}
 
 
+int veryfy(){
 
-
-
+  int n1 = Sendsms(contact[0],testmsg);   // send sms and get return (if sent, return 1)
+  int n2 = Sendsms(contact[1],testmsg);
+  
+  if(n1==1 and n2==1 ){
+    #if defined(PrintSerialMonitor)
+    Serial.println("numbers veryfied");
+    #endif
+    return 1;
+    
+  }else {
+     #if defined(PrintSerialMonitor)
+    Serial.println("numbers veryfy fail");
+    #endif
+    return 0;
+    
+    }
+}
 
 
 int CheckNetwork(){
 
   sim.print("AT\r\n");
   delay(500);
-  while(!sim.available());   //reasy gsm
-
-  while(sim.available()){
-   //Serial.print((char)sim.read());
-   int temp = (char)sim.read();    // get garbadge values
-  } 
+  while(!sim.available());    //ready gsm
   
-  sim.print("AT+CREG?\r\n"); 
+  while(sim.available()){
+    //Serial.print((char)sim.read());
+    int temp = (char)sim.read();    // get garbadge values
+    }
+    
+  sim.print("AT+CREG?\r\n");
   delay(500);
   while(!sim.available());
 
   String response;
   while(sim.available()){
-  response.concat((char)sim.read());
+    response.concat((char)sim.read());
     
-  }
-
-//Serial.println(response);               // responce format +CREG: 1,1
-//Serial.println(response.charAt(18));
-//Serial.println(response.charAt(20));
-
-if(response.charAt(18)=='0' or response.charAt(18)=='6'){  // char 18 and 20 became 0,1 on home network 
-  if(response.charAt(20)=='1'){
+    }
     
-    Serial.println("Network Ok");
-    return 1;
+  //Serial.println(response);     // responce format +CREG: 1,1
+  //Serial.println(response.charAt(18));
+  //Serial.println(response.charAt(20));
+  
+  if(response.charAt(18)=='0' or response.charAt(18)=='6'){ //char 18 and 20 became 0,1 on home network
+    if(response.charAt(20)=='1'){
+       #if defined(PrintSerialMonitor)
+      Serial.println("Network Ok");
+      #endif
+      return 1;
+        
     }else{
+       #if defined(PrintSerialMonitor)
       Serial.println("Network Fail");
+      #endif
       return 0;
       }
+      
   }else{
+     #if defined(PrintSerialMonitor)
     Serial.println("Network Fail");
+    #endif
     return 0;
-    }  
-  
-  }
+    }
+    
+}
 
 
 
 int StoreContact(){
   
-Serial.println("Reading contact");
+   #if defined(PrintSerialMonitor)
+  Serial.println("Reading contact");
+  #endif
+  
+  contact[0]=Readcontact(1);  // Get contact numbers and store
+  contact[1]=Readcontact(2);
 
-contact[0]=Readcontact(1);  // Get contact numbers and store
-contact[1]=Readcontact(2);
-
-Serial.println(contact[0]);
-Serial.println(contact[1]);
-
-if(contact[0]=="N" or contact[1]=="N"){
-  Serial.println("Reading Err");
-  return 0;
-  }else{
-
-    Serial.println("Contact Stored");
-    return 1;
+   #if defined(PrintSerialMonitor)
+  Serial.println(contact[0]);
+  Serial.println(contact[1]);
+  #endif
+  
+  if(contact[0]=="N" or contact[1]=="N"){
+     #if defined(PrintSerialMonitor)
+    Serial.println("Reading Err");
+    #endif
+    return 0;
     
+  }else{
+     #if defined(PrintSerialMonitor)
+    Serial.println("Contact Stored");
+    #endif
+    return 1;
     }
-  
-  
-  
-  
-  }
+}
 
 
 int InitGsm(){
-  
-  int gsm=0;
 
+  int gsm=0;
+  
   sim.print("AT\r\n");
   delay(500);
   while(!sim.available());  //wait for responce gsm
   
+  String response;
   while(sim.available()){
- 
-   // Serial.print((char)sim.read());
-   if((char)sim.read()=='O'){
-    if((char)sim.read()=='K'){
-      gsm=1;  //working Gsm
-      
-      }
+    response.concat((char)sim.read());  //responce string have AT +OK 
+    }
     
+  String cmd = response.substring(6,8); //substring OK part
+  if(!(cmd.indexOf("OK")==-1)){   //if true Gsm return ok
+    gsm=1;
     }
-   
-  } 
-
-if(gsm==1){
-  Serial.println("Gsm working");
-  }else{
-    Serial.println("Gsm Fail..!");
-    }
-
+    
   return gsm;
   
-  
-  }
-
-
-
-
-
-
+}
 
 
 int Sendsms(String num, String msg){
 
-
   sim.print("AT\r\n");
   delay(500);
-  while(!sim.available());   //ready gsm
-
-  while(sim.available()){     // garbadge collection
-   //Serial.print((char)sim.read());
-   int temp = (char)sim.read();
-  }
+  while(!sim.available());  //ready gsm
   
-  sim.print("AT+CMGF=1\r\n");    // Text Mode
+  while(sim.available()){   //garbadge collection
+    //Serial.print((char)sim.read());
+    int temp = (char)sim.read();
+    }
+    
+  sim.print("AT+CMGF=1\r\n");   // Text Mode
   delay(1000);
   
   sim.print("AT+CMGS=\"");
@@ -364,53 +469,47 @@ int Sendsms(String num, String msg){
   
   sim.println(msg);
   delay(500);
-  sim.println((char)26);//  CTRL+Z  for end msg
+  sim.println((char)26);    //CTRL+Z  for end msg
   delay(500);
-
-
-  while(sim.available()){
-   //Serial.print((char)sim.read());
-   int a = (char)sim.read();
-  }
-
+    
+  #if defined(PrintSerialMonitor)
   Serial.print("Msg sent to ");
   Serial.print(num);
   Serial.print(" \r\n");
- 
+  #endif
   
   return 1;
   
-  }
-
-
+}
 
 
 String Readcontact(int index){
-  
+
   sim.print("AT\r\n");
   delay(500);
   while(!sim.available());   //ready gsm
-
+  
   while(sim.available()){       //  collect garbage
-  // Serial.print((char)sim.read());
+    // Serial.print((char)sim.read());
     int a = (char)sim.read();
-  } 
+    
+    }
 
-  sim.print("AT+CPBR="); 
+  sim.print("AT+CPBR=");
   sim.print(index);
   sim.print("\r\n");
   delay(500);
   
   while(!sim.available());
-
+  
   String response;
   while(sim.available()){
-   response.concat((char)sim.read());
-   //Serial.print(ss);
+    response.concat((char)sim.read());
+    //Serial.print(ss);
+    
+    }
+    
+  String phoneno = response.substring(22, 32);  // get contact no from string
+  return phoneno;
   
 }
-
- String phoneno = response.substring(22, 32);  // get contact no from string
- return phoneno;
- 
-  }
